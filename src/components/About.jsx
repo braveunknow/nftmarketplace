f1f0a5ec028c1b03f30bf729473564817f9575f9
bhushan mayekar <bhushan.hermanos@gmail.com>
2023-04-17 13:35:23 +0530

    Project Completed

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam tenetur, architecto expedita, impedit dignissimos error repudiandae quis, facilis fugit veniam pariatur corrupti sunt nesciunt blanditiis a voluptas adipisci? Possimus totam aut repellat omnis sequi praesentium officia in quasi neque molestias incidunt, deleniti quidem similique ex debitis facere minus id! Optio.</div>
+    </div>
+  )
+}
+
+export default About
